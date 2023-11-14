8b8fda0998e30ab4ad0f2f44c01f34766fcc8f6b
Kanishk Singh <33978645+sinkanishk@users.noreply.github.com>
2023-11-13 19:25:28 -0500

    Update script.js

--- Photo_Album/frontend/script.js
+++ Photo_Album/frontend/script.js
@@ -1,107 +1,101 @@
-// Constants for API endpoints
+// Define the base URL for the API and specific endpoints
 const API_BASE_URL = 'https://7n40rj72sl.execute-api.us-east-1.amazonaws.com/deploycf';
 const SEARCH_ENDPOINT = '/search';
 const UPLOAD_ENDPOINT = '/upload/b2store/';
 
-// Function to make HTTP requests to the API
-async function makeRequest(url, options) {
-  try {
-    const response = await fetch(url, options);
-    return response.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Function to handle displaying "No photos found" message
-function displayNoResultsMessage() {
-  const photosDiv = document.getElementById("photos_search_results");
-  photosDiv.innerHTML = "";
-
-  const message = document.createElement('p');
-  message.textContent = 'No photos found';
-  photosDiv.appendChild(message);
-}
-
-// Function to render photos
-function renderPhotos(imagePaths) {
-  const photosDiv = document.getElementById("photos_search_results");
-  photosDiv.innerHTML = "";
-
-  for (let i = 0; i < imagePaths.length; i++) {
-    const photo_path = imagePaths[i];
-
-    const photoHtml = `<figure style="display:inline-block; margin:10px; width:calc(100%/3 - 20px)">
-                          <img src="${photo_path}" style="width:100%">
-                          <figcaption style="text-align:center">${photo_path.split('/')[3].split('?')[0]}</figcaption>
-                      </figure>`;
-    photosDiv.innerHTML += photoHtml;
-  }
-}
-
 // Function to handle text search
 function textSearch() {
+  // Get the search query from the input field
   const searchQuery = document.getElementById('search_query').value;
-  const url = `${API_BASE_URL}${SEARCH_ENDPOINT}?q=${searchQuery}`;
 
-  const options = {
+  // Make an HTTP GET request to the search endpoint with the search query as a parameter
+  fetch(API_BASE_URL + SEARCH_ENDPOINT + '?q=' + searchQuery, {
     method: 'GET',
     headers: {
       "x-api-key": "IjkeYboLIQ8CkngFOhIzO4SiGSCXONa76N7SVPlj",
     },
-  };
-
-  makeRequest(url, options)
+  })
+    .then(response => response.json())
     .then(data => {
       console.log(data);
+      // Handle the case when no results are found
       if (data === 'No Results found') {
-        displayNoResultsMessage();
+        var photosDiv = document.getElementById("photos_search_results");
+        photosDiv.innerHTML = "";
+        const message = document.createElement('p');
+        message.textContent = 'No photos found';
+        photosDiv.appendChild(message);
       } else {
-        renderPhotos(data.imagePaths);
+        // Display search results in the specified HTML element
+        var photosDiv = document.getElementById("photos_search_results");
+        photosDiv.innerHTML = "";
+
+        // Iterate through the image paths and display each photo
+        for (let i = 0; i < data.imagePaths.length; i++) {
+          const photo_path = data.imagePaths[i];
+
+          const photoHtml = '<figure style="display:inline-block; margin:10px; width:calc(100%/3 - 20px)">' +
+            '<img src="' + `${photo_path}` + '" style="width:100%">' +
+            '<figcaption style="text-align:center">' + photo_path.split('/')[3].split('?')[0] + '</figcaption>' +
+            '</figure>';
+          photosDiv.innerHTML += photoHtml;
+        }
       }
-    });
+    })
+    .catch(error => console.log(error));
 }
 
 // Function to perform a voice search
 function voiceSearch() {
+  // Access the mic icon and create a new speech recognition object
   const $micIcon = $('#mic_search');
   const recognition = new webkitSpeechRecognition();
 
-  recognition.onstart = function() {
+  // Set up event handlers for the speech recognition
+  recognition.onstart = function () {
     $micIcon.text('mic_off');
   };
 
-  recognition.onresult = function(event) {
+  recognition.onresult = function (event) {
+    // Get the transcript from the speech recognition result and perform a text search
     const query = event.results[0][0].transcript;
     $('#search_query').val(query);
     textSearch();
   };
 
-  recognition.onerror = function(event) {
+  recognition.onerror = function (event) {
     console.error(event.error);
   };
 
-  recognition.onend = function() {
+  recognition.onend = function () {
     $micIcon.text('mic');
   };
 
+  // Start the speech recognition process
   recognition.start();
 }
 
 // Function to handle photo upload
 function uploadPhoto() {
+  // Get the uploaded file and custom labels from the input fields
   const uploadedFile = document.getElementById('uploaded_file').files[0];
   const customLabels = document.getElementById('custom_labels').value;
-  const fileName = uploadedFile.name;
-  const fileType = fileName.split('.').pop();
 
+  // Create a new FormData object and append the uploaded file and custom labels to it
   const formData = new FormData();
   formData.append('file', uploadedFile);
   formData.append('labels', customLabels);
 
-  const url = `${API_BASE_URL}${UPLOAD_ENDPOINT}${fileName}`;
+  // Get the file name, type, and log them for debugging purposes
+  const fileInput = document.getElementById('uploaded_file');
+  const fileName = fileInput.value.split('\\').pop();
+  const fileType = fileName.split('.').pop();
+  console.log(fileName);
+  console.log('file type', fileType);
+  console.log('cusLab', customLabels);
 
-  const options = {
+  // Make an HTTP PUT request to the upload endpoint with the file and custom labels as parameters
+  fetch(API_BASE_URL + UPLOAD_ENDPOINT + fileName, {
     method: 'PUT',
     headers: {
       "x-amz-meta-customLabels": customLabels,
@@ -109,8 +103,8 @@ function uploadPhoto() {
       "x-api-key": "IjkeYboLIQ8CkngFOhIzO4SiGSCXONa76N7SVPlj"
     },
     body: uploadedFile
-  };
-
-  makeRequest(url, options)
-    .then(data => console.log(data));
+  })
+    .then(response => response)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 }
